87e2749970b8b7e1756d32c090a30092fdb941e0
stardust <stardustfxprivate@gmail.com>
2022-12-16 19:48:19 -0800

    Finish options page for defaultLength

--- options.js
+++ options.js
@@ -1,25 +1,46 @@
 function save_options() {
-    var length = document.getElementById('default-length').value;
-  
-    console.log(length);
-    // chrome.storage.sync.set({
-    //     defaultLength: length
-    // }, function() {
-    //     var status = document.getElementById('status');
-    //     status.textContent = 'Options saved';
-    //     setTimeout(function() {
-    //         status.textContent = '';
-    //     }, 750);
-    // });
+    let defaultLengthBox = document.getElementById('default-length');
+    let statusMessage = document.getElementById('status');
+    let length = parseInt(defaultLengthBox.value);
+
+    if (isNaN(length)) return highlightError(defaultLengthBox);
+    if (length <= 0 || length > 50) return highlightError(defaultLengthBox);
+
+    chrome.storage.local.set({
+        defaultLength: length
+    }, function() {
+        statusMessage.textContent = 'Options saved';
+        defaultLengthBox.style.borderColor = "rgba(0, 0, 0, 0.25)"; 
+        setTimeout(function() {
+            statusMessage.textContent = '';
+        }, 750);
+    });
 }
 
 function restore_options() {
-    // chrome.storage.sync.get({
-    //     defaultLength: 16
-    // }, function() {
-    //     document.getElementById('default-length').value = 16;
-    // });
+    let statusMessage = document.getElementById('status');
+
+    chrome.storage.local.set({
+        defaultLength: 16
+    }, function() {
+        document.getElementById('default-length').value = 16;
+        statusMessage.textContent = 'Restored defaults';
+        setTimeout(function() {
+            statusMessage.textContent = '';
+        }, 750);
+    });
 }
 
-document.addEventListener('DOMContentLoaded', restore_options)
+function highlightError(element) {
+    element.style.borderColor = 'red';
+}
+
+function load() {
+    chrome.storage.local.get('defaultLength', function(result) {
+        document.getElementById('default-length').value = result.defaultLength;
+    })
+}
+
+document.addEventListener('DOMContentLoaded', load)
 document.getElementById('save').addEventListener('click', save_options);
+document.getElementById('restore').addEventListener('click', restore_options);
