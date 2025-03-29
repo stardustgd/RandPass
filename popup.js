7e03990f6e84992fb8eee96251acd6dfc858c6e3
stardust <88991140+stardustgd@users.noreply.github.com>
2025-03-29 13:07:47 -0700

    feat: improve random password generation
    include more special characters and set default length to 25

--- popup.js
+++ popup.js
@@ -1,63 +1,64 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var angle = 0;
-    let passField = document.getElementById('password-field');
-    let generateButton = document.getElementById('generate-button');
-    let slider = document.getElementById('length-slider');
-    let sliderValue = document.getElementById('length-value');
+document.addEventListener("DOMContentLoaded", function () {
+  var angle = 0;
+  let passField = document.getElementById("password-field");
+  let generateButton = document.getElementById("generate-button");
+  let slider = document.getElementById("length-slider");
+  let sliderValue = document.getElementById("length-value");
 
-    chrome.storage.local.get([
-        'defaultLength',
-        'hidePassword'
-    ], function(result) {
-        // Handle default length
-        let passLength = parseInt(result.defaultLength);
-        passField.value = generatePassword(passLength);
-        sliderValue.innerHTML = passLength;
-        slider.value = passLength;
+  chrome.storage.local.get(
+    ["defaultLength", "hidePassword"],
+    function (result) {
+      // Handle default length
+      let passLength = parseInt(result.defaultLength) || 25;
+      passField.value = generatePassword(passLength);
+      sliderValue.innerHTML = passLength;
+      slider.value = passLength;
 
-        // Handle hidePassword
-        if (result.hidePassword)
-            document.getElementById('password-field').type = 'password';
-    });
-    
-    generateButton.addEventListener('click', function() {
-        passField.value = generatePassword(sliderValue.innerHTML);
-        angle -= 180;
+      // Handle hidePassword
+      if (result.hidePassword)
+        document.getElementById("password-field").type = "password";
+    },
+  );
 
-        document.getElementById('generate-icon').style.transform = `rotate(${angle}deg)`;
-    });
+  generateButton.addEventListener("click", function () {
+    passField.value = generatePassword(sliderValue.innerHTML);
+    angle -= 180;
 
-    document.getElementById('copy-button').addEventListener('click', function() {
-        passField.select()
-        passField.setSelectionRange(0, 99999);
-        navigator.clipboard.writeText(passField.value);
-        window.getSelection().removeAllRanges();
+    document.getElementById("generate-icon").style.transform =
+      `rotate(${angle}deg)`;
+  });
 
-        let tooltip = document.getElementById('textTooltip');
-        tooltip.innerHTML = "Copied!";
-        document.getElementById('copy-icon').style.color = 'royalblue';
+  document.getElementById("copy-button").addEventListener("click", function () {
+    passField.select();
+    passField.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(passField.value);
+    window.getSelection().removeAllRanges();
 
-        setTimeout(() => {
-            tooltip.innerHTML = "Copy";
-            document.getElementById('copy-icon').style.color = 'black';
-        }, "2000");
-    });
+    let tooltip = document.getElementById("textTooltip");
+    tooltip.innerHTML = "Copied!";
+    document.getElementById("copy-icon").style.color = "royalblue";
 
-    slider.oninput = function() {
-        sliderValue.innerHTML = this.value;
-        passField.value = generatePassword(sliderValue.innerHTML);
-    }
+    setTimeout(() => {
+      tooltip.innerHTML = "Copy";
+      document.getElementById("copy-icon").style.color = "black";
+    }, "2000");
+  });
+
+  slider.oninput = function () {
+    sliderValue.innerHTML = this.value;
+    passField.value = generatePassword(sliderValue.innerHTML);
+  };
 });
 
 function generatePassword(length = 16) {
-    let chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*'
-    let array = new Uint32Array(chars.length);
-    let password = '';
+  let chars =
+    '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*[]{}()/\\_<>-+=".,?';
+  let array = new Uint32Array(chars.length);
+  let password = "";
 
-    window.crypto.getRandomValues(array);
-    
-    for (let i = 0; i < length; i++)
-        password += chars[array[i] % chars.length];
+  window.crypto.getRandomValues(array);
 
-    return password;
+  for (let i = 0; i < length; i++) password += chars[array[i] % chars.length];
+
+  return password;
 }
