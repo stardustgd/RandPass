fc813c874fd91f3625de287f16a262a40ac8bd43
stardust <88991140+stardustgd@users.noreply.github.com>
2025-03-29 13:07:47 -0700

    feat: improve random password generation
    include more special characters and set default length to 25

--- options.js
+++ options.js
@@ -1,53 +1,59 @@
 function save_options() {
-    let defaultLengthBox = document.getElementById('default-length');
-    let statusMessage = document.getElementById('status');
-    let length = parseInt(defaultLengthBox.value);
-    let hidePassword = false;
+  let defaultLengthBox = document.getElementById("default-length");
+  let statusMessage = document.getElementById("status");
+  let length = parseInt(defaultLengthBox.value);
+  let hidePassword = false;
 
-    if (isNaN(length)) return highlightError(defaultLengthBox);
-    if (length <= 0 || length > 50) return highlightError(defaultLengthBox);
+  if (isNaN(length)) return highlightError(defaultLengthBox);
+  if (length <= 0 || length > 50) return highlightError(defaultLengthBox);
 
-    if (document.getElementById('hidePassword').checked)
-        hidePassword = true;
+  if (document.getElementById("hidePassword").checked) hidePassword = true;
 
-    chrome.storage.local.set({
-        defaultLength: length,
-        hidePassword: hidePassword
-    }, function() {
-        statusMessage.textContent = 'Options saved';
-        defaultLengthBox.style.borderColor = "rgba(0, 0, 0, 0.25)"; 
-        setTimeout(function() {
-            statusMessage.textContent = '';
-        }, 750);
-    });
+  chrome.storage.local.set(
+    {
+      defaultLength: length,
+      hidePassword: hidePassword,
+    },
+    function () {
+      statusMessage.textContent = "Options saved";
+      defaultLengthBox.style.borderColor = "rgba(0, 0, 0, 0.25)";
+      setTimeout(function () {
+        statusMessage.textContent = "";
+      }, 750);
+    },
+  );
 }
 
 function restore_options() {
-    let statusMessage = document.getElementById('status');
+  let statusMessage = document.getElementById("status");
 
-    chrome.storage.local.set({
-        defaultLength: 16,
-        hidePassword: false
-    }, function() {
-        document.getElementById('default-length').value = 16;
-        document.getElementById('hidePassword').checked = false;
-        statusMessage.textContent = 'Restored defaults';
-        setTimeout(function() {
-            statusMessage.textContent = '';
-        }, 750);
-    });
+  chrome.storage.local.set(
+    {
+      defaultLength: 25,
+      hidePassword: false,
+    },
+    function () {
+      document.getElementById("default-length").value = 16;
+      document.getElementById("hidePassword").checked = false;
+      statusMessage.textContent = "Restored defaults";
+      setTimeout(function () {
+        statusMessage.textContent = "";
+      }, 750);
+    },
+  );
 }
 
 function highlightError(element) {
-    element.style.borderColor = 'red';
+  element.style.borderColor = "red";
 }
 
 function load() {
-    chrome.storage.local.get('defaultLength', function(result) {
-        document.getElementById('default-length').value = result.defaultLength;
-    })
+  chrome.storage.local.get("defaultLength", function (result) {
+    document.getElementById("default-length").value =
+      result.defaultLength || 25;
+  });
 }
 
-document.addEventListener('DOMContentLoaded', load)
-document.getElementById('save').addEventListener('click', save_options);
-document.getElementById('restore').addEventListener('click', restore_options);
+document.addEventListener("DOMContentLoaded", load);
+document.getElementById("save").addEventListener("click", save_options);
+document.getElementById("restore").addEventListener("click", restore_options);
